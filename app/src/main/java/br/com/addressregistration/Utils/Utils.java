3ebd62abdc85ff5c1f8fd27626f716125988ab35
ana204 <anagomes.lucia@hotmail.com>
2022-07-05 13:08:19 -0300

    file utils

--- app/src/main/java/br/com/addressregistration/Utils/Utils.java
+++ app/src/main/java/br/com/addressregistration/Utils/Utils.java
@@ -1,8 +1,8 @@
 package br.com.addressregistration.Utils;
 
 public class Utils {
-    public static final String URL_USERS = "http:///list/";
-    public static final String URL_POST = "http:///";
+    public static final String URL_USERS = "http://:8081/list/";
+    public static final String URL_POST = "http://:8081/addUsers/";
     public static final String BASE_URL_VIA_CEP = "https://viacep.com.br/ws/";
 
 }
